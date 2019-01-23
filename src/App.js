ec345d5eb25973d5bf4ab9a1cafcc87b3fc4c939
Cory Hoban <hobancor@gmail.com>
2019-01-23 16:58:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,37 @@ import WeatherChart from "./WeatherChart";
 import WeatherData from "./WeatherData";
 import Timeline from "./Timeline/Timeline";
 import { TimelineData } from "./Timeline/TimelineData";
+ipmort ScrollChart from "./ScrollChart";
+
+const ScrollData = [
+	{ activity: "Main Idea and Detail", value: 90, color: "#2A78E4" },
+	{ activity: "Character and Plot", value: 80, color: "#2A78E4" },
+	{ activity: "Elements of Poetry", value: 70, color: "#2A78E4" },
+	{ activity: "Standard 8.10", value: 60, color: "#2A78E4" },
+	{ activity: "8.1.3", value: 50, color: "#2A78E4" },
+	{ activity: "Skill 6", value: 40, color: "#2A78E4" },
+	{ activity: "Skill 7", value: 30, color: "#2A78E4" },
+	{ activity: "Skill 8", value: 21, color: "#2A78E4" },
+	{ activity: "Skill 9", value: 10, color: "#2A78E4" },
+	{ activity: "Skill 10", value: 5, color: "#2A78E4" },
+	{ activity: "8.1.34", value: 50, color: "#2A78E4" },
+	{ activity: "Skill 60", value: 40, color: "#2A78E4" },
+	{ activity: "Skill 70", value: 30, color: "#2A78E4" },
+	{ activity: "Skill 80", value: 21, color: "#2A78E4" },
+	{ activity: "Skill 90", value: 10, color: "#2A78E4" },
+	{ activity: "Skill 100", value: 5, color: "#2A78E4" },
+	{ activity: "Skill 900", value: 100, color: "#2A78E4" },
+	{ activity: "Skill 1000", value: 50, color: "#2A78E4" },
+	{ activity: "Skill -1", value: 5, color: "#2A78E4" },
+	{ activity: "8.1.35", value: 50, color: "#2A78E4" },
+	{ activity: "Skill 160", value: 40, color: "#2A78E4" },
+	{ activity: "Skill 10-2", value: 30, color: "#2A78E4" },
+	{ activity: "Skill 20", value: 21, color: "#2A78E4" },
+	{ activity: "Skill 80-2", value: 10, color: "#2A78E4" },
+	{ activity: "Skill 650", value: 5, color: "#2A78E4" },
+	{ activity: "Skill 300", value: 100, color: "#2A78E4" },
+	{ activity: "Skill 3000", value: 50, color: "#2A78E4" }
+];
 
 class App extends Component {
   render() {
@@ -19,6 +50,7 @@ class App extends Component {
           <BarChart data={[5, 10, 1, 3]} size={[500, 700]} />
           <WeatherChart data={WeatherData.list} />
           <Timeline data={TimelineData} />
+          <ScrollChart />
         </div>
       </div>
     );
